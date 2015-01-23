5929117eb05183a8fa7923371768b1846d99b296
TheEnvoy <marcel.zons@googlemail.com>
2015-01-23 20:11:10 +0100

    Data

---
+++ data.js
@@ -0,0 +1,27 @@
+
+data =			{
+					currentLevel:		0,
+					life:				10,
+					waypoints:			[],
+					
+					enemies:			{
+											human1:			{
+																hitpoints:	1,
+																speed:		0.2,
+																image:		"",
+																flying:		0,
+																resistence:	"",
+															},
+										},
+					towers:				{
+											tower1:			{
+																costs:		[1, 1, 1, 1],
+																level:		0,
+																damage:		[1, 1, 1, 1],
+																firerate:	[1, 1, 1, 1],
+																range:		[],
+																damageType:	"",
+																special:	"",
+															},
+										},
+				};
