4f00d6c86e7150299b45eac755b1954673890d27
nivida <samuel.furter@brack.ch>
2015-01-24 00:04:49 +0100

    getWaypointsPosition

--- map.js
+++ map.js
@@ -1,16 +1,61 @@
 /**
- * Returns Map HTML string
- * @var mapdata JSON
+ * @param mapData
  */
 function renderMap(mapData)
 {
-    var i = 1;
+    var i = 0;
     var map = "";
+    var tiles = tileIdToClass(mapData);
 
-    mapData.forEach(function (tile) {
-            map = map + "<div id='" + i +"' class='"+ tile +"' data-waypoint='false'></div>";
-            i++;
+    tiles.forEach(function (tile) {
+        map = map + "<div id='" + i +"' class='"+ tile +"'></div>";
+        i++;
     });
 
     $('#map').append(map);
+}
+
+/**
+ *
+ * @param mapData TileIDs
+ * @returns {Array} tiles
+ */
+function tileIdToClass(mapData)
+{
+    var tiles = [];
+    mapData.forEach(function(tile) {
+        if(tile == 1)
+        {
+            tiles.push('wegHorizontal');
+        } else if(tile == 2)
+        {
+            tiles.push('wegVertikal');
+        }
+    });
+
+    return tiles;
+}
+
+/**
+ * @returns {Array} waypointsWithPosition
+ * @param waypoints
+ */
+function getWaypointsPosition(waypoints)
+{
+    var tileHeight = 32;
+    var tileWidth = 32;
+    var waypointsWithPosition = [];
+
+    waypoints.forEach(function(waypoint) {
+        var multiplikator = Math.floor((waypoint / 20));
+        var positions = [];
+
+        positions['id'] = waypoint;
+        positions['top'] = tileHeight * multiplikator;
+        positions['left'] = waypoint%20 * tileWidth;
+
+        waypointsWithPosition.push(positions);
+    });
+
+    return waypointsWithPosition;
 }
