31e6a46a1707ccf01a51f89fe6f27aefcee84688
TheEnvoy <marcel.zons@googlemail.com>
2015-01-25 13:58:33 +0100

    ...

--- helpers.js
+++ helpers.js
@@ -501,3 +501,31 @@ function spawnTower(offsetTop, offsetLeft, towerName)
         $('#objects').append(tower);
 
 }
+
+var sound = {
+	sampels: [],
+	mp3: function() 
+	{
+		var audio = new Audio();
+		return !!audio.canPlayType && audio.canPlayType('audio/mpeg') != "";
+	},
+	
+	play: function( file ) 
+	{
+		var self = this;
+		//if(this.mp3()) file = file.replace(/ogg/g, "mp3");
+		if( !this.sampels[file] )
+			this.sampels[file] = [];
+		if( this.sampels[file].length ) 
+		{
+			var sound = this.sampels[file].pop();
+			sound.play();
+			return sound;
+		} else {
+			var sound = new Audio( file );
+			sound.onended = function() { self.sampels[file].push( this ); };
+			sound.play();
+			return sound;
+		}
+	},
+}
